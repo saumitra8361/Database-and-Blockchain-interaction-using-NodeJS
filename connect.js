2382dda418053592f2e77e896497f92952871eca
Saumitra Bajpai <saumitra0210@gmail.com>
2019-04-15 11:45:06 +0530

    exploring db connection

--- connect.js
+++ connect.js
@@ -7,25 +7,25 @@ var dbConfig = {
   password: 'testid01',
 };
 
-  // connect to your database
-  sql.connect(dbConfig, function (err) {
-      console.log("connecting.....");
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Connected");
-  /*      // create Request object
-        var request = new sql.Request();
-        // query to the database and get the records
-        request.query('select * from TestSchema.Employees', function (err, recordset) {
-            if (err) {
-              console.log(err);
-              sql.close();
-            } else {
-              // send records as a response
-              console.log(recordset);
-              sql.close();
-            }
-        }); */
-      }
-  });
+// connect to your database
+sql.connect(dbConfig, function (err) {
+    console.log("connecting.....");
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected");
+/*      // create Request object
+      var request = new sql.Request();
+      // query to the database and get the records
+      request.query('select * from TestSchema.Employees', function (err, recordset) {
+          if (err) {
+            console.log(err);
+            sql.close();
+          } else {
+            // send records as a response
+            console.log(recordset);
+            sql.close();
+          }
+      }); */
+    }
+});
